75fb18d81b7879989173146e99e799dc552b0345
Ramon Figueiredo <rfigueiredo@imageworks.com>
2025-02-18 00:27:23 -0800

    Add download_images.sh

--- data/download_images.sh
+++ data/download_images.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 # URL of the zip file containing images
-ZIP_URL="https://example.com/path/to/your/images.zip"
+ZIP_URL="https://drive.google.com/uc?id=1VS9CQgCZXbXdEystdpYRf3c9GeSSRQ33"
 
 # Directory to extract images to
 EXTRACT_DIR="."
 
-# Download the zip file
+# Download the zip file using gdown
 echo "Downloading $ZIP_URL..."
-curl -o "$EXTRACT_DIR/images.zip" "$ZIP_URL"
+gdown "$ZIP_URL" -O "$EXTRACT_DIR/images.zip"
 
 # Extract the zip file
-echo "Extracting to $EXTRACT_DIR..."
+echo "Extracting images"
 unzip "$EXTRACT_DIR/images.zip" -d "$EXTRACT_DIR"
 
 # Remove the zip file
 echo "Removing the zip file..."
 rm "$EXTRACT_DIR/images.zip"
+rm -r __MACOSX
 
 echo "Done."
